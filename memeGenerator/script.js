3c22fb78abd4082c3f4c7ee3aab6bd10df697f63
Lesego <160973669+LesegoKh@users.noreply.github.com>
2025-02-09 05:54:23 +0200

    Update script.js
    particles JS

--- memeGenerator/script.js
+++ memeGenerator/script.js
@@ -81,6 +81,60 @@ function updateMeme() {
     downloadButton.style.display = "block";
 }
 
+// Trialing library Particles.js code from docs
 
-
-
+particlesJS("particles-js", {
+    particles: {
+        number: {
+            value: 100,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            value: "#ffffff"
+        },
+        shape: {
+            type: "circle"
+        },
+        opacity: {
+            value: 0.5,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0
+            }
+        },
+        size: {
+            value: 5,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 3,
+                size_min: 0.1
+            }
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1
+        }
+    },
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: {
+                enable: true,
+                mode: "repulse"
+            },
+            onclick: {
+                enable: true,
+                mode: "push"
+            }
+        }
+    }
+});
